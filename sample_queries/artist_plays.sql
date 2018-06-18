c8851f3403103ff5b598885e41d586d3e0d3515b
Adrien Delessert <adrien@metriccollective.com>
2018-06-18 12:27:26 -0400

    Add artist plays query

--- sample_queries/artist_plays.sql
+++ sample_queries/artist_plays.sql
@@ -1,13 +1,13 @@
--- Like the artist_plays query, gets the total number of plays per album
+-- Get the total number of plays per artist, with some descriptive stats on the distribution of
+-- plays across the artist's tracks.
 
 select
     artist,
-    album,
     sum(play_count) as total_plays,
     max(play_count) as most_played_count,
     round(avg(play_count)) as average_count,
     min(play_count) as least_played_count
 from tracks
-group by artist, album
+group by artist
 order by total_plays desc
 limit 50;
